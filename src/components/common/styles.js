5940cbe67e1e52b83bc305b1c9b8a7611a657754
Joanne Daudier <jdaudier@outlook.com>
2017-03-19 08:02:14 -0400

    Styling updates

--- src/components/common/styles.js
+++ src/components/common/styles.js
@@ -14,8 +14,8 @@ background: linear-gradient(to left, #D31027 , #EA384D);
 
 export const Header = styled.h1`
 	background: #8e9eab;
-	background: -webkit-linear-gradient(to left, #8e9eab , #eef2f3);
-	background: linear-gradient(to left, #8e9eab , #eef2f3);
+	background: -webkit-linear-gradient(to left, #bdc1c5 , #eef2f3);
+	background: linear-gradient(to left, #bdc1c5 , #eef2f3);
 	box-shadow: 0 3px 3px 0 rgba(0,0,0,0.14), 0 1px 7px 0 rgba(0,0,0,0.12), 0 3px 1px -1px rgba(0,0,0,0.2);
     color: #363637;
 	text-align: center;
@@ -25,10 +25,9 @@ export const Header = styled.h1`
 `;
 
 export const Button = styled.button`
-
 	background-color: ${props => props.gradient === 'light' ? '#8e9eab' : '#D31027'};
-	background: ${props => props.gradient === 'light' ? '-webkit-linear-gradient(to left, #8e9eab , #eef2f3)' : '-webkit-linear-gradient(to left, #D31027 , #EA384D)'};
-	background: ${props => props.gradient === 'light' ? 'linear-gradient(to left, #8e9eab , #eef2f3)' : 'linear-gradient(to left, #D31027 , #EA384D)'};
+	background: ${props => props.gradient === 'light' ? '-webkit-linear-gradient(to left, #bdc1c5 , #eef2f3)' : '-webkit-linear-gradient(to left, #D31027 , #EA384D)'};
+	background: ${props => props.gradient === 'light' ? 'linear-gradient(to left, #bdc1c5 , #eef2f3)' : 'linear-gradient(to left, #D31027 , #EA384D)'};
 	border: none;
 	border-radius: ${props => props.borderRadius ? props.borderRadius : '4px'};
 	box-shadow: ${props => props.boxShadowUpwards ? '0 -3px 3px 0 rgba(0,0,0,0.14), 0 1px 7px 0 rgba(0,0,0,0.12), 0 -3px 1px -1px rgba(0,0,0,0.2)' : '0 3px 3px 0 rgba(0,0,0,0.14), 0 1px 7px 0 rgba(0,0,0,0.12), 0 3px 1px -1px rgba(0,0,0,0.2)'};
@@ -57,5 +56,5 @@ export const Row = styled.div`
 
 export const SVG = styled.svg`
 	padding-top: ${props => props.addTopPadding ? '40px' : 'none'}
-	height: ${props => props.fitToScreenHeight ? 'calc(100vh - 150px)' : 'auto'}
+	height: ${props => props.fitToScreenHeight ? 'calc(100vh - 180px)' : 'auto'}
 `;
