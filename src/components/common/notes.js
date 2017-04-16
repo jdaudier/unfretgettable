c5700e7b1364cc95ecc63317dc1fabf39d94481d
Joanne Daudier <jdaudier@outlook.com>
2017-04-15 22:46:45 -0400

    Add E, F, G on 6th string and 3 F#

--- src/components/common/notes.js
+++ src/components/common/notes.js
@@ -1,4 +1,4 @@
-export const notes = ['a5s', 'b5s2f', 'd4s', 'e4s2f', 'f4s3f', 'g3s', 'a3s2f', 'b2s', 'c2s1f', 'd2s3f', 'e1s', 'f1s1f', 'g1s3f', 'c5s3f'];
+export const notes = ['a5s', 'e6s', 'f6s1f', 'f6s2f', 'g6s3f',  'b5s2f', 'd4s', 'e4s2f', 'f4s3f', 'f4s4f', 'g3s', 'a3s2f', 'b2s', 'c2s1f', 'd2s3f', 'e1s', 'f1s1f', 'f1s2f', 'g1s3f', 'c5s3f'];
 
 export const noteNameMapping = {
 	a5s: 'A',
@@ -15,4 +15,10 @@ export const noteNameMapping = {
 	e1s: 'E',
 	f1s1f: 'F',
 	g1s3f: 'G',
+	e6s: 'E',
+	f6s1f: 'F',
+	g6s3f: 'G',
+	'f6s2f': 'F#',
+	'f4s4f': 'F#',
+	'f1s2f': 'F#',
 };
