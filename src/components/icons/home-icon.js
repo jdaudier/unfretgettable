0aae2b512104ab6442aed5620f86e1e6ec170df0
Joanne Daudier <jdaudier@outlook.com>
2017-05-26 23:37:58 -0400

    Fix styling bug

--- src/components/icons/home-icon.js
+++ src/components/icons/home-icon.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const SVG = styled.svg`
-	height: 24px
+	height: 24px;
 	position: absolute;
 	right: 10px;
 	top: 3px;
