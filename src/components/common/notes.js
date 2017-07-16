19fb0b4e67f42b9aa0adfdd448a7bdb476fdeca5
Joanne Daudier <jdaudier@outlook.com>
2017-07-16 04:05:22 -0400

    Add C#

--- src/components/common/notes.js
+++ src/components/common/notes.js
@@ -1,9 +1,10 @@
-export const notes = ['a5s', 'e6s', 'f6s1f', 'f6s2f', 'g6s3f',  'b5s2f', 'd4s', 'e4s2f', 'f4s3f', 'f4s4f', 'g3s', 'a3s2f', 'b2s', 'c2s1f', 'd2s3f', 'e1s', 'f1s1f', 'f1s2f', 'g1s3f', 'c5s3f'];
+export const notes = ['a5s', 'e6s', 'f6s1f', 'f6s2f', 'g6s3f',  'b5s2f', 'd4s', 'e4s2f', 'f4s3f', 'f4s4f', 'g3s', 'a3s2f', 'b2s', 'c2s1f', 'd2s3f', 'e1s', 'f1s1f', 'f1s2f', 'g1s3f', 'c5s3f', 'c2s2f'];
 
 export const noteNameMapping = {
 	a5s: 'A',
 	b5s2f: 'B',
 	c5s3f: 'C',
+	c2s2f: 'C#',
 	d4s: 'D',
 	e4s2f: 'E',
 	f4s3f: 'F',
