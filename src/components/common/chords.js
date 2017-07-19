37c84ff23a8ef818db1ce9fce03fbde0a3f4f674
Joanne Daudier <jdaudier@outlook.com>
2017-07-19 15:51:13 -0400

    Show all notes for chords

--- src/components/common/chords.js
+++ src/components/common/chords.js
@@ -27,12 +27,12 @@ export const chordToNotesMapping = {
 };
 
 export const standardNotationChordToNotesMapping = {
-	C: ['g3s', 'e4s2f', 'c5s3f'],
-	G7: ['f1s1f', 'b2s', 'g3s', 'd4s'],
-	G: ['g1s3f', 'd4s', 'b5s2f'],
+	C: ['e1s', 'c2s1f', 'g3s', 'e4s2f', 'c5s3f'],
+	G7: ['f1s1f', 'b2s', 'g3s', 'd4s', 'b5s2f', 'g6s3f'],
+	G: ['g1s3f', 'b2s', 'g3s', 'd4s', 'b5s2f', 'g6s3f'],
 	D7: ['f1s2f3fin', 'c2s1f', 'a3s2f', 'd4s'],
-	Em: ['b2s', 'g3s', 'e4s2f3fin'],
-	D: ['f1s2f', 'd2s3f', 'a3s2f1fin'],
-	A7: ['e1s', 'c2s2f', 'g3s', 'a5s'],
+	Em: ['e1s', 'b2s', 'g3s', 'e4s2f3fin', 'b5s2f', 'e6s'],
+	D: ['f1s2f', 'd2s3f', 'a3s2f1fin', 'd4s'],
+	A7: ['e1s', 'c2s2f', 'g3s', 'e4s2f1fin', 'a5s'],
 };
 
