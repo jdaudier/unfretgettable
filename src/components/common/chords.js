4c7a8d97bcd620bc6cace5ac6beed734ba629591
Joanne Daudier <jdaudier@outlook.com>
2017-07-19 01:13:12 -0400

    Add 2nd pattern for G

--- src/components/common/chords.js
+++ src/components/common/chords.js
@@ -11,7 +11,8 @@ export const chords = [
 export const chordToNotesMapping = {
 	C: ['e1s', 'c2s1f', 'g3s', 'e4s2f', 'c5s3f', 'x6s'],
 	G7: ['f1s1f', 'b2s', 'g3s', 'd4s', 'b5s2f', 'g6s3f'],
-	G: ['g1s3f4fin', 'b2s', 'g3s', 'd4s', 'b5s2f', 'g6s3f'],
+	// G: ['g1s3f4fin', 'b2s', 'g3s', 'd4s', 'b5s2f', 'g6s3f'],
+	G: ['g1s3f', 'b2s', 'g3s', 'd4s', 'b5s2f1fin', 'g6s3f2fin'],
 	D7: ['f1s2f3fin', 'c2s1f', 'a3s2f', 'd4s', 'x5s', 'x6s'],
 	Em: ['e1s', 'b2s', 'g3s', 'e4s2f3fin', 'b5s2f', 'e6s'],
 	D: ['f1s2f', 'd2s3f', 'a3s2f1fin', 'd4s', 'x5s', 'x6s'],
@@ -21,7 +22,7 @@ export const chordToNotesMapping = {
 export const standardNotationChordToNotesMapping = {
 	C: ['g3s', 'e4s2f', 'c5s3f'],
 	G7: ['f1s1f', 'b2s', 'g3s', 'd4s'],
-	G: ['g3s', 'd4s', 'b5s2f'],
+	G: ['g1s3f', 'd4s', 'b5s2f'],
 	D7: ['f1s2f3fin', 'c2s1f', 'a3s2f', 'd4s'],
 	Em: ['b2s', 'g3s', 'e4s2f3fin'],
 	D: ['f1s2f', 'd2s3f', 'a3s2f1fin'],
