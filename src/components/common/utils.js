ea122b937b0dc13a0d079d9e8f3a13600e615e81
Joanne Daudier <joanne.daudier@zapier.com>
2019-04-07 03:35:07 -0400

    Switch to vexchords

--- src/components/common/utils.js
+++ src/components/common/utils.js
@@ -2,6 +2,6 @@ import {chordToNotesMapping} from "./chords";
 
 export function getPatternCount(chordString) {
 	const chord = chordToNotesMapping[chordString];
-	const {chordSpelling, startingFret, ...rest} = chord;
+	const {chordSpelling, position, ...rest} = chord;
 	return Object.keys(rest).length;
 }
